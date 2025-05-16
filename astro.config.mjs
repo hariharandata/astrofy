1a2ef2bcd51733e17827945a4c4faeeda5728772
Hariharan Gopinath <tharanihari26@gmail.com>
2025-05-16 16:30:11 +0200

    edit files

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,3 +8,15 @@ export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
   integrations: [mdx(), sitemap(), tailwind()]
 });
+
+
+// import { defineConfig } from 'astro/config';
+// import mdx from '@astrojs/mdx';
+// import sitemap from '@astrojs/sitemap';
+// import tailwind from "@astrojs/tailwind";
+
+// // https://astro.build/config
+// export default defineConfig({
+//   site: 'https://hariharandata.github.io',
+//   base: 'astrofy',
+// });
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE = {
+    name: "Hariharan Gopinath",
+    title: "Data Engineer | Cloud & ML Enthusiast",
+    github: "https://github.com/hariharandata",
+    linkedin: "https://linkedin.com/in/hariharan-gopinath-290a51169/",
+    email: "tharanihari26@gmail.com",
+    // etc.
+  };
